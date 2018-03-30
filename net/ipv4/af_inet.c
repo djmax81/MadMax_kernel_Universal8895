1351d91103d17a238e5f3a4db0f3c545d766323b
djmax81 <djmax81@gmail.com>
2018-03-29 21:55:12 -0600

    net: make KNOX_NCM disabable
    Signed-off-by: djmax81 <djmax81@gmail.com>

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -424,12 +424,14 @@ out_rcu_unlock:
 
 /* START_OF_KNOX_NPA */
 /** The function is used to check if the ncm feature is enabled or not; if enabled then it calls knox_collect_socket_data function in ncm.c to record all the socket data; **/
+#ifdef CONFIG_KNOX_NCM
 static void knox_collect_metadata(struct socket *sock) {
     if(check_ncm_flag()) {
     	knox_collect_socket_data(sock);
     }
 }
 /* END_OF_KNOX_NPA */
+#endif
 
 /*
  *	The peer socket should always be NULL (or else). When we call this
@@ -460,9 +462,11 @@ int inet_release(struct socket *sock)
 		if (sock_flag(sk, SOCK_LINGER) &&
 		    !(current->flags & PF_EXITING))
 			timeout = sk->sk_lingertime;
+#ifdef CONFIG_KNOX_NCM
         /* START_OF_KNOX_NPA */
         knox_collect_metadata(sock);
         /* END_OF_KNOX_NPA */
+#endif
 		sock->sk = NULL;
 		sk->sk_prot->close(sk, timeout);
 	}
